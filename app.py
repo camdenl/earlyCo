49ac714e6932ea5e3464c862273549654e035709
camdenl <camdenl@uga.edu>
2014-12-02 12:22:10 -0200

    init

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
 __author__ = 'camden'
 
-# A very simple Flask Hello World app for you to get started with...
 
 from flask import render_template, request, Response, Flask, jsonify
 import os
+import geocoding as gc
+from geojson import dumps, loads
+
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    try:
+        with open('static/features.json', 'r') as gj:
+            geoj = loads(gj.read())
+        return render_template('index.html', geoj = geoj)
+    except IOError:
+        return render_template('index.html', geoj = 0)
+@app.route('/feature', methods=['GET'])
+def feature():
+    lat = request.args.get('lat', 1, type=float)
+    lng = request.args.get('lng', 1, type=float)
+    name = request.args.get('name', 1, type=str)
+    addr = request.args.get('addr', 1, type=str)
+    loc = request.args.get('loc', 1, type=str)
+    f = gc.create_feature(lat, lng, name, addr, loc)
+    resp = gc.create_geojson(f)
+    return jsonify(result = f)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- geocoding.py
+++ geocoding.py
@@ -1,7 +1,8 @@
 __author__ = 'camden'
 
-from geopy.geocoder import GoogleV3 as goog
-import geojson
+from geopy.geocoders import GoogleV3 as goog
+from geojson import Feature, Point, FeatureCollection, loads, dumps
+import os
 
 def point_from_string(address):
     """
@@ -10,6 +11,56 @@ def point_from_string(address):
     :param address: string representing address to geocode
     :return: array of [lat, long] coordinates
     """
-    geoloc = goog()
-    loc = geoloc.geocode(address)
-    try
+    try:
+        geoloc = goog()
+        loc = geoloc.geocode(address)
+        return [loc.latitude, loc.longitude]
+    except Exception:
+        return None
+
+
+def create_feature(lat, lng, name, address, loc):
+    """
+    create a geojson feature given an address string and the loc of feature
+
+    :param address: comma delimited string in form 'address, city, state zip, country'
+    :param loc: string representing the data loc: 'dropbox, resource, sign'
+    :return: geojson.feature object
+    """
+
+    if loc not in ['dropbox', 'resource', 'sign']:
+        raise ValueError('invalid loc')
+
+    properties = {
+        'loc': loc,
+        'name': name,
+        'addr': address
+    }
+
+    f = Feature(geometry=Point([lng,lat]), properties=properties)
+    return f
+
+
+def create_geojson(feature):
+    crs = {
+    "type": "name",
+    "properties": {
+        "name": "EPSG:4326"
+        }
+    }
+
+    try:
+        if os.path.exists('static/features.json'):
+            with open('static/features.json', 'r') as gj:
+                feat_coll = gj.read()
+            feat_coll = loads(feat_coll)
+            feats = feat_coll.features
+            feats.append(feature)
+            with open('static/features.json', 'w') as gj:
+                gj.write(dumps(feat_coll))
+        else:
+            with open('static/features.json', 'w') as gj:
+                gj.write(dumps(FeatureCollection([feature], crs = crs)))
+        return 'ok'
+    except Exception:
+        return 'error'
